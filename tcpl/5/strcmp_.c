f79dd86861c16fa165bcf4ac95de0db5ea0c8df1
nokdoot@naver.com <nokdoot@naver.com>
2017-02-17 01:04:08 +0900

    20170217

---
+++ tcpl/5/strcmp_.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int strcmp_(char const *s, char const *t){
+	
+	extern int fold;
+	printf("%d\n", fold);
+	for(; (fold) ? (tolower(*s) == tolower(*t)) : (*s == *t); *s++, *t++){
+		if(*s == '\0')
+			return 0;
+	}
+
+	return (fold) ? (tolower(*s) - tolower(*t)) : (*s - *t);
+}	
