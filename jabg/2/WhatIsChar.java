2127fce3b47f469ad21c3a6c04b44c0d96df2dec
nokdoo <nokdoot@naver.com>
2017-05-19 02:10:13 +0900

    osc

--- jabg/2/WhatIsChar.java
+++ jabg/2/WhatIsChar.java
@@ -1,11 +1,11 @@
 class WhatIsChar{
 	public static void main(String args[]){
-		char c = 0;
+		char c = 'X';
 		char c2 = 95;
 
 		System.out.println(c2);
 		System.out.println(c);
 		System.out.println(c + c2);
-		System.out.println(c-1);
+		System.out.println("문자와 결합되면  " + (c));
 	}
 }
