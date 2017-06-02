406c1108bd582f334f7c90ee71ec606c658f5a3e
nokdoo <nokdoot@naver.com>
2017-06-03 02:19:45 +0900

    jabg

---
+++ jabg/6/TestMethod.java
@@ -0,0 +1,10 @@
+
+public class TestMethod {
+
+	void test(){
+		System.out.println("haha");
+		TestStatic ts = new TestStatic();
+		System.out.println(TestStatic.a);
+	}
+	
+}
---
+++ jabg/6/TestStatic.java
@@ -0,0 +1,10 @@
+class TestStatic {
+	
+	static int b = 25;
+	static{
+		System.out.println(b);
+		//a = 20;
+		int a = Math.abs(10);
+		System.out.println(a);
+	}
+}
