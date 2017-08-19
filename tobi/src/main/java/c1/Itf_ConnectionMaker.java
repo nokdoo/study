53a699b109313556e65dfc420968c6d4bd5d4500
SEO Youn-Taek <nokdoot@naver.com>
2017-08-19 20:17:36 +0900

    tobi

--- tobi/src/main/java/c1/Itf_ConnectionMaker.java
+++
@@ -1,7 +0,0 @@
-package c1;
-
-import java.sql.SQLException;
-
-public interface Itf_ConnectionMaker {
-	java.sql.Connection Connection() throws ClassNotFoundException, SQLException;
-}
--- tobi/src/main/java/c1/datasource/DataSource.java
+++ tobi/src/main/java/c1/datasource/DataSource.java
@@ -6,8 +6,6 @@ import java.sql.Wrapper;
 
 import javax.sql.CommonDataSource;
 
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-
 public interface DataSource extends CommonDataSource, Wrapper{
 	Connection getConnection() throws SQLException;
 }
