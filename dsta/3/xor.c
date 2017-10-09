1957376bcd9a873101536b3d79eeed87c676fed6
SEO Youn-Taek <nokdoot@naver.com>
2017-10-10 00:37:03 +0900

    dsta

--- dsta/3/xor.c
+++ dsta/3/xor.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 struct Node{
 	int data;
 	struct Node* XORaddr;
 };
 
 struct Node *XOR(struct Node *a, struct Node *b){
-	return (struct Node *) ((unsigned int) (a) ^ (unsigned int) (b));
+	return (struct Node *) ((uintptr_t) (a) ^ (uintptr_t) (b));
 }
 
-struct Node *insertNode(struct Node **head, int data){
+void *insertNode(struct Node **head, int data){
 	struct Node *tmp_node = (struct Node *) malloc(sizeof(struct Node));
 	tmp_node->data = data;
 
-	tmp_node->XORaddr = XOR((*head)->XORaddr, NULL);
+	tmp_node->XORaddr = XOR(*head, NULL);
 
 	if(*head != NULL){
 		struct Node *next = XOR((*head)->XORaddr, NULL);
 		(*head)->XORaddr = XOR(tmp_node, next);
 	}
 
-	*head = tmp;
+	*head = tmp_node;
+}
+
+void printxor(struct Node *head){
+	struct Node *cur = head;
+	struct Node *pre = NULL;
+	struct Node *next;
+
+	while(cur != NULL){
+		printf("%d ", cur->data);
+		next = XOR(pre, cur->XORaddr);
+		
+		pre = cur;
+		cur=next;
+	}
 }
 
 int main(){
@@ -29,4 +44,5 @@ int main(){
 	insertNode(&head, 2);
 	insertNode(&head, 3);
 	insertNode(&head, 4);
+	printxor(head);
 }
