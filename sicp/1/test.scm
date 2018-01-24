ee3b49cd49351dfee4877323c32a589e5631ccf9
nokdoo <nokdoot@naver.com>
2018-01-24 10:37:44 +0900

    sicp

--- sicp/1/test.scm
+++ sicp/1/test.scm
@@ -1,1 +1,11 @@
-(next 1)
+(lambda (x) (+ x 4))
+
+(lambda (x) (/ 1.0 (* x (+ x 2))))
+
+(define (pi-sum a b)
+	(sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+		a
+		(lambda (x) (+ x 4))
+		b))
+
+(pi-sum 2 4)
