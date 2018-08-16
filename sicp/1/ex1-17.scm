308fdf1cee74eff0682a16ecf1499f750359c622
nokdoo <nokdoot@naver.com>
2018-08-17 01:35:54 +0900

    restart sicp

--- sicp/1/ex1-17.scm
+++
@@ -1,9 +0,0 @@
-(define (multi a n)
-	(multi-iter a n 0))
-
-(define (multi-iter a n p)
-	(cond ((= n 0) p)
-		((even? n) (multi-iter (+ a a) (/ n 2) p))
-		(else (multi-iter a (- n 1) (+ a p)))))
-
-(multi 3 150000)
--- sicp/1/fib.scm
+++
@@ -1,8 +0,0 @@
-(define (fib n)
-	(fib-iter 0 1 n))
-
-(define (fib-iter a b count)
-	(cond ((= count 0) a)
-		(else (fib-iter b (+ a b) (- count 1)))))
-
-(fib 10)
