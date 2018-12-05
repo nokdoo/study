f13aa03e1024087a0e140efd8c4f4f95cdad28d5
nokdoot <nokdoot@naver.com>
2018-12-05 12:17:46 +0900

    algo

--- algo/hackerrank/Video_Conference.pl
+++ algo/hackerrank/Video_Conference.pl
@@ -3,61 +3,46 @@
 use strict;
 use warnings;
 
-sub diff_index {
-	my ($str1, $str2, $len) = @_;
-
-	$len = 1 unless defined $len;
-
-	my $len1 = length $str1;
-	my $len2 = length $str2;
-	my $min_len = ($len1 < $len2) ? $len1 : $len2;
-
-	while ($len <= $min_len){
-		return $len-1 unless (substr($str1, 0, $len) eq substr($str2, 0, $len));
-		$len++;
-	}
-
-	return $len-1;
-}
-
-sub shortest_prefix {
-	my ($str, $hash) = @_;
-	my $len_str = length $$str;
-	my $len_prefix = 1;
-
-	while ($len_prefix <= $len_str){
-		my $substr = substr $$str, 0, $len_prefix;
-		unless ( exists $$hash{$substr} ) {
-			print "\t\t$$str $substr\n";
-			return $substr;
-		}
-		$len_prefix = diff_index($$hash{$substr}, $$str, $len_prefix)+1;
-	}
-	print "\t\t$$str $$str\n";
-	return $$str;
-}
-
 sub solve {
-	my ($arr) = @_;
-	my %hash;
+	my ($arr_ref) = @_;
+	my %hash1;
 	my %hash2;
-	$hash{_same}=\%hash2;
 
-	foreach my $str (@$arr){
-		my $prefix = shortest_prefix(\$str, \%hash);
-		if( exists $hash{$prefix} ) {
-			$hash2{$prefix}++;
-			print "$prefix $hash2{$prefix}\n";
-		}else{
-			$hash2{$prefix} = 1;
-			print "$prefix\n";
-			$hash{$prefix} = $str;
+	foreach my $str (@$arr_ref)
+	{
+		my $len = length $str;
+		my $flag = 0;
+		for(1..$len)
+		{
+			my $word = substr($str, 0, $_);
+			unless (exists $hash2{$word})
+			{
+				$hash2{$word} = 0;
+				if ($flag == 0 && !(exists $hash1{$word}))
+				{
+					$flag = 1;
+					$hash1{$word} = 1;	
+					print "$word\n";
+				}
+			}
+
+			if($_ == $len)
+			{
+				$hash2{$str}++; 
+				if ($flag == 0)
+				{
+					if ($hash2{$str}>1)
+					{
+						print "$str $hash2{$str}\n";
+					}
+					else
+					{
+						print "$str\n";
+					}
+				}
+			}
 		}
 	}
-
-	delete $hash{_same};
-
-	return \%hash;
 }
 
 my $n = <>;
